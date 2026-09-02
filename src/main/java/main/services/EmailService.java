291446657b5323d31ea189348b59efcb00a84c20
sayan0503 <sayan.roy.bca27@tha.edu.in>
2026-09-02 22:37:10 +0530

    Email services disabled temporarily

--- src/main/java/main/services/EmailService.java
+++ src/main/java/main/services/EmailService.java
@@ -1,50 +1,15 @@
 package main.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
-
     public void sendRegistrationEmail(String email, String name) {
-
-        SimpleMailMessage message = new SimpleMailMessage();
-
-        message.setTo(email);
-        message.setSubject("Welcome to StoreMate");
-        message.setText(
-                "Hello " + name + ",\n\n" +
-                "Welcome to StoreMate!\n\n" +
-                "Your account has been successfully created.\n\n" +
-                "You can now use StoreMate to manage your stores, inventory, sales, revenue and profit.\n\n" +
-                "Thank you for using StoreMate.\n\n" +
-                "Regards,\n" +
-                "StoreMate"
-        );
-
-        mailSender.send(message);
+        // Email temporarily disabled
     }
 
     public void sendDeletionEmail(String email, String name) {
-
-        SimpleMailMessage message = new SimpleMailMessage();
-
-        message.setTo(email);
-        message.setSubject("StoreMate Account Deleted");
-        message.setText(
-                "Hello " + name + ",\n\n" +
-                "Your StoreMate account has been successfully deleted.\n\n" +
-                "All associated account data has been removed according to the account deletion process.\n\n" +
-                "If you did not request this deletion, please contact us.\n\n" +
-                "Regards,\n" +
-                "StoreMate"
-        );
-
-        mailSender.send(message);
+        // Email temporarily disabled
     }
 }
